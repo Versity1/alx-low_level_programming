a5b51ce740f0b4bdd256ef0897f221830dcedce9
Versity ICT Solutions <vgsicthub@gmail.com>
2022-09-08 21:53:12 -0700

    details

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,17 @@
  */
 
 int main(void)
-
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 	return (0);
 }
