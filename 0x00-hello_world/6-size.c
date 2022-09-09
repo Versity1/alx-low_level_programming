9ae27620c9a8200217f07a39b04c4ee2a2593a8b
Versity ICT Solutions <vgsicthub@gmail.com>
2022-09-08 22:28:17 -0700

    new changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,7 @@
  * Description: this program checks the size of 
  * Return: 0
  */
+
 int main(void)
 {
 	char c;
@@ -13,6 +14,7 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
