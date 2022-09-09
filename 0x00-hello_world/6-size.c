424c7b7fcd9dbdee45791bc3ce7843fab506ffa7
Versity ICT Solutions <vgsicthub@gmail.com>
2022-09-08 21:43:07 -0700

    checking the size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- print the string function sizeof()
+ *
+ * Description: this program checks the size of 
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
