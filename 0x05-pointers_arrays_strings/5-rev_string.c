f4536955fff7998233323f276867246d774444d4
Versity ICT Solutions <90280527+Versity1@users.noreply.github.com>
2022-09-19 16:12:00 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,26 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s[10] = "My School";
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
 
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 <= 14; n2++)
+		{
+			if (n2 > 9)
+			{
+				putchar((n2 / 10) + '0');
+			}
+			putchar((n2 % 10) + '0');
+		}
+		putchar(10);
+	}
 }
