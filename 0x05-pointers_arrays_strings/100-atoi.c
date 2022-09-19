4057685b34afcdac0281ba7d8e8bb519c74acb7e
Versity ICT Solutions <90280527+Versity1@users.noreply.github.com>
2022-09-19 16:21:05 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,41 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
-*print_triangle - prints a triangle
-*@size:size parameter of triangle
-*Return: returns nothing
-*/
-
-void print_triangle(int size)
+ * _atoi - this function return the legth of a string
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+int _atoi(char *s)
 {
-	int inc1, inc2;
+	int i = 0;
+	unsigned int result = 0;
+	int negative = 0;
 
-	if (size > 0)
+	while (s[i] != '\0')
 	{
-		for (inc1 = 1; inc1 <= size; inc1++)
+		if (s[i] == '-')
 		{
-			for ((inc2 = size - inc1); inc2 > 0; inc2--)
-			{
-				putchar(' ');
-			}
-
-			for (inc2 = 0; inc2 < inc1; inc2++)
-			{
-				putchar('#');
-			}
-
-			if (inc1 == size)
-			{
-				continue;
-			}
-
-			putchar('\n');
+			negative++;
 		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + s[i] - '0';
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+		}
+		if (s[i] > '9' && s[i] < 0)
+		{
+			return (0);
+		}
+		i++;
 	}
-	putchar('\n');
+	if (result != 0 && (negative % 2 != 0 || negative == 1))
+	{
+		result = result * -1;
+	}
+	return (result);
 }
