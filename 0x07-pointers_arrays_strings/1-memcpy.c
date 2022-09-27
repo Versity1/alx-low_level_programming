f72e5103a698a24eb288d95218e228f9e0f300a9
Versity ICT Solutions <90280527+Versity1@users.noreply.github.com>
2022-09-27 03:25:43 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -22,18 +22,3 @@ char *_memcpy(char *dest, char *src, unsigned int n)
                                                                                                                                         
         return (dest);                                                                                                                  
 }
-#include "main.h" 
-/**                                                                                                                                     
-
- * *_memcpy -  copies memory area from src to dest                                                                                      
-
- * @dest: destination                                                                                                                   
-
- * @src: source                                                                                                                         
-
- * @n: max bytes to use                                                                                                                 
-
- * Return: dest                                                                                                                         
-
-*/
-
